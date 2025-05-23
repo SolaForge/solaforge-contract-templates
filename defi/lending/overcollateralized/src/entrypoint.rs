254de83923a9e78dcfce097caee81cf021b7ad17
Avelynmitchell <solaforge1@gmail.com>
2025-05-23 14:54:10 +0800

    add lending templates

---
+++ defi/lending/overcollateralized/src/entrypoint.rs
@@ -0,0 +1,17 @@
+//! Program entrypoint definition
+use crate::process_instruction;
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
+
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction);
+
+/// Program entrypoint's implementation
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    process_instruction(program_id, accounts, instruction_data)
+}
